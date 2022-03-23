feed7dfb6d82d344681d35ec471b30458b573908
Satoshi Sugai <sensan.3104sato@gmail.com>
2022-03-23 20:08:07 +0900

    mod

--- models/vgg13.py
+++ models/vgg13.py
@@ -1,63 +1,52 @@
+'''VGG11/13/16/19 in Pytorch.'''
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
-class VGG13(nn.Module):
-    def __init__(self, num_classes=10, init_weights=True, dropout=0.5, **kwargs):
-        super(VGG13, self).__init__()
-        self.layer1 = self._make_layer(3, 64)
-        self.layer2 = self._make_layer(64, 128)
-        self.layer3 = self._make_layer(128, 256)
-        self.layer4 = self._make_layer(256, 512)
-        self.layer5 = self._make_layer(512, 512)
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 512),
-            nn.ReLU(True),
-            nn.Dropout(p=dropout),
-            nn.Linear(512, 32),
-            nn.ReLU(True),
-            nn.Dropout(p=dropout),
-            nn.Linear(32, num_classes),
-        )
-        if init_weights:
-            for m in self.modules():
-                if isinstance(m, nn.Conv2d):
-                    nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
-                elif isinstance(m, nn.BatchNorm2d):
-                    nn.init.constant_(m.weight, 1)
-                    nn.init.constant_(m.bias, 0)
-                elif isinstance(m, nn.Linear):
-                    nn.init.normal_(m.weight, 0, 0.01)
-                    nn.init.constant_(m.bias, 0)
-    
-    def _make_layer(self, in_channels, dim):
-        conv2d_1 = nn.Conv2d(in_channels, dim, kernel_size=3, padding=1)
-        conv2d_2 = nn.Conv2d(dim, dim, kernel_size=3, padding=1)
-        layers = [
-            conv2d_1,
-            nn.ReLU(inplace=True),
-            conv2d_2,
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)]
-        return nn.Sequential(*layers)
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
 
-def vgg13(**kwargs):
-    return VGG13(**kwargs)
-    
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
 
 
-    
+def vgg13(pretrained = False, progress = True, **kwargs):
+    r"""VGG 13-layer model (configuration "B")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_.
+    The required minimum input size of the model is 32x32.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return VGG('VGG11')
 
